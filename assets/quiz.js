614ff69775a4f5ec044aac28a920ffc11a7fc247
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-01-08 01:48:45 -0500

    Sequenced questions with objects

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,57 +1,76 @@
+var card = document.getElementById("card");
+var clickBtn = document.getElementById("button");
+var index = 0;
+
 var startScreen = [
-    {
-        instructions: 'Please click the below to start the game.',
-        button: 'Start Game'
-    }
+  {
+    instructions: "Please click the below to start the game.",
+    button: "Start Game",
+  },
 ];
 
-var quizContent = [
-    {
-        question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula.',
-        correct: 'Right Answer',
-        wrongOne: 'Wrong One',
-        wrongTwo: 'Wrong Two',
-        wrongThree: 'Wrong Three'
-    },
-    {
-        question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula.',
-        correct: 'Right Answer',
-        wrongOne: 'Wrong One',
-        wrongTwo: 'Wrong Two',
-        wrongThree: 'Wrong Three'
-
-    },
-    {
-        question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula.',
-        correct: 'Right Answer',
-        wrongOne: 'Wrong One',
-        wrongTwo: 'Wrong Two',
-        wrongThree: 'Wrong Three'
-
-    },
-    {
-        question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula.',
-        correct: 'Right Answer',
-        wrongOne: 'Wrong One',
-        wrongTwo: 'Wrong Two',
-        wrongThree: 'Wrong Three'
-
-    },
+var quizQuestions = [
+  {
+    question:
+      "What is today's date?",
+    answers: ["Right", "Wrong", "Wrong", "Wrong"],
+  },
+  {
+    question:
+      "Who won the Super Bowl last year?",
+    answers: ["Right", "Wrong", "Wrong", "Wrong"],
+  },
+  {
+    question:
+      "When did the Fat Boys bereak up?",
+    answers: ["Right", "Wrong", "Wrong", "Wrong"],
+  },
+  {
+    question:
+      "Happy ____ , Happy Life",
+    answers: ["Right", "Wrong", "Wrong", "Wrong"],
+  },
+  {
+    question:
+      "How much was this boot camp?",
+    answers: ["Right", "Wrong", "Wrong", "Wrong"],
+  },
 ];
 
-var para = document.createElement('p')
-var startButton = document.createElement('button');
+function startGame() {
+  var p = document.createElement("p");
 
-function startGameScreen(){
-    para.innerHTML = startScreen[0].instructions;
-    document.getElementById('question-area').appendChild(para);
-    startButton.innerHTML = startScreen[0].button;
-    document.getElementById('question-area').appendChild(startButton);
+  p.textContent = startScreen[0].instructions;
+
+  card.append(p);
+
+  var btn = document.createElement("button");
+  btn.textContent = startScreen[0].button;
+  btn.addEventListener("click", renderQuestion);
+  btn.setAttribute("id", "start-button");
+  p.append(btn);
 }
 
-startGameScreen();
+function renderQuestion() {
+  card.innerHTML = "";
+  var p = document.createElement("p");
 
-document.getElementsByClassName('card');
+  p.textContent = quizQuestions[index].question;
+  card.append(p);
+  for (var i = 0; i < quizQuestions[index].answers.length; i++) {
+    var btn = document.createElement("button");
+    btn.textContent = quizQuestions[index].answers[i];
+    btn.setAttribute("id", "button");
+    btn.addEventListener("click", click);
+    card.append(btn);
+  }
+  console.log(index);
+}
 
-var timer = document.getElementById('countdown');
+function click() {
+  index++;
 
+  renderQuestion();
+}
+
+startGame();
